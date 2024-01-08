375f7901d45f22a0b47cebdc5b7de4d331241440
King <tmdwns563@naver.com>
2024-01-08 13:37:53 +0900

    TodoList

---
+++ src/AboutPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from 'styled-components';
+import HomePage from "./HomePage";
+const AboutPage = () => {
+
+    return (
+        <div>
+            <HomePage/>
+        </div>
+    )
+}
+
+export default AboutPage;
